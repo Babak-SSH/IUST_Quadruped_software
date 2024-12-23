0b0d864d99caecd7d6c6a041ef4dcde8c6c89c2b
Babak-SSH <babak.sefidgar@gmail.com>
2024-12-23 11:44:49 +0330

    DEL: remove repetitive definitions

--- robot/include/rt/rt_joystick_interface.h
+++ robot/include/rt/rt_joystick_interface.h
@@ -13,18 +13,6 @@
 #include <linux/joystick.h>
 
 
-class rc_control_settings {
-  public:
-    double     mode;
-    double     p_des[2]; // (x, y) -1 ~ 1
-    double     height_variation; // -1 ~ 1
-    double     v_des[3]; // -1 ~ 1 * (scale 0.5 ~ 1.5)
-    double     rpy_des[3]; // -1 ~ 1
-    double     omega_des[3]; // -1 ~ 1
-    double     variable[3];
-    double     step_height;
-};
-
 void get_js_control_settings(void* settings);
 
 int receive_data(int fd);
